54c20d82b275a9ff01031abfbee1c4b94ac9fe6e
unknown <khan.farhan1985@gmail.com>
2023-10-17 10:37:55 +0530

    Added GitDemo project to local repository

---
+++ src/main/java/com/example/mypack/GitDemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.mypack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Repository
+public class GitDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDemoApplication.class, args);
+	}
+	
+	@GetMapping(path = "/welcome")
+	public String getMessage() {
+		return "This is First Git Demo API";
+	}
+
+}
